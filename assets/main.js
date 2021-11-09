947c6a967550730b568b7d70ed2837128547f730
Virgiawan <virgiawan150@gmail.com>
2021-11-10 01:33:22 +0700

    Add files via upload

---
+++ assets/main.js
@@ -0,0 +1,17 @@
+const nav = document.querySelector("#nav");
+
+window.addEventListener("scroll", (event) => {
+  const scrollY = window.scrollY;
+  if (scrollY < 20) {
+    nav.classList.remove("shadow-bottom");
+  } else {
+    nav.classList.add("shadow-bottom");
+  }
+});
+
+const menu = document.querySelector(".menu");
+const menuButton = document.querySelector(".menu-button");
+
+menuButton.addEventListener("click", (event) => {
+  menu.classList.toggle("open");
+});
